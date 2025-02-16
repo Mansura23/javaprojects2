23db433759101102917a325d60a1817160d15d44
Mansura <badalovamansura40@gmail.com>
2025-02-16 19:43:26 +0400

    first project attached

--- src/main/java/az/edu/turing/module2/lesson01/Arrays.java
+++ src/main/java/az/edu/turing/module2/lesson01/Arrays.java
@@ -5,19 +5,21 @@ import java.util.Scanner;
 
 public class Arrays {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        int num=scanner.nextInt();
+
+        Scanner scanner = new Scanner(System.in);
+
+        int num = scanner.nextInt();
         System.out.println(num);
-        int[] arr=new int[num];
-        for (int i=0;i<num;i++){
-            arr[i]=scanner.nextInt();
+        int[] arr = new int[num];
+        for (int i = 0; i < num; i++) {
+            arr[i] = scanner.nextInt();
 
 
         }
-        int maxNum=arr[0];
-        for (int i=1; i< arr.length;i++){
-            if (arr[i]>maxNum){
-                maxNum=arr[i];
+        int maxNum = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > maxNum) {
+                maxNum = arr[i];
 
             }
         }
